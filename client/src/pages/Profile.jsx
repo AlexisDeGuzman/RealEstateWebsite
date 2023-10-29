218e98dec0b7f81bad828569441373a88e2e8123
AlexisDeGuzman <124367138+AlexisDeGuzman@users.noreply.github.com>
2023-10-29 12:16:36 +0800

    complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,13 +1,100 @@
 import { useSelector } from 'react-redux';
+import { useEffect, useRef, useState } from 'react';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../firebase';
 
 export default function Profile() {
   const { currentUser } = useSelector(state => state.user);
+  const fileRef = useRef(null);
+  const [ file, setFile ] = useState(undefined);
+  const [ filePerc, setFilePerc ] = useState(0);
+  const [ fileUploadError, setFileUploadError ] = useState(false);
+  const [ formData, setFormData ] = useState({});
+
+  // firebase storage
+  // allow read;
+  // allow write: if 
+  // request.resource.size < 2* 1024 * 1024 &&
+  // request.resource.contentType.matches('image/.*')
+
+  const handleFileUpload = file => {
+    // the app comes from the firebase.js
+    const storage = getStorage(app);
+    // make a unique fileName
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+
+    // Upload the file to Firebase Storage
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed', 
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+
+      // set an error to true if there is any
+      (error) => {
+        setFileUploadError(true);
+      },
+
+      // This function runs when the upload is complete and successful
+      // Get the download URL of the uploaded file
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref)
+          .then(downloadURL => {
+            // Set the download URL in the formData state
+            setFormData({ ...formData, avatar: downloadURL});
+          })
+      }
+    );
+  }
+
+  useEffect(() => {
+    if(file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
-        <img src={currentUser.avatar} alt="profile" className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'/>
+        <input 
+          onChange={(e) => setFile(e.target.files[0])} 
+          type="file" 
+          ref={fileRef} 
+          hidden 
+          accept='image/*'
+        />
+        <img 
+          onClick={() => fileRef.current.click()} 
+          src={formData.avatar || currentUser.avatar} 
+          alt="profile" 
+          className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'
+        />
+        <p className='text-sm self-center'>
+          {fileUploadError 
+            ? 
+            <span className='text-red-700'>
+              Error Image Upload(Image must be less than 2mb)
+            </span>
+            :
+            filePerc > 0 && filePerc < 100
+            ? 
+            <span className='text-slate-700'>
+              {`Uploading ${filePerc}%`}
+            </span>
+            :
+            filePerc === 100
+            ?
+            <span className='text-green-700'>
+              Image successfully uploaded!
+            </span>
+            :
+            ""
+          }
+        </p>
         <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' />
         <input type="email" placeholder={currentUser.email} className='border p-3 rounded-lg' id='email' />
         <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' />
