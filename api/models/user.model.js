0ffc60121fb8b1f72feb3994fde3bf5c25c3271d
AlexisDeGuzman <124367138+AlexisDeGuzman@users.noreply.github.com>
2023-11-18 17:56:53 +0800

    add delete user functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
     },
     avatar: {
         type: String,
-        defualt: "https://i.pinimg.com/originals/f1/0f/f7/f10ff70a7155e5ab666bcdd1b45b726d.jpg",
+        default: "https://i.pinimg.com/originals/f1/0f/f7/f10ff70a7155e5ab666bcdd1b45b726d.jpg",
     },
 }, {timestamps: true});
 
